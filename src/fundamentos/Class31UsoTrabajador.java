e5c6d83794de936069cc3501f7b76781bb8ca10c
Serra <pacoserranox@gmail.com>
2020-12-09 16:49:57 +0100

    Merge origin/master
    Conflicts:
    	src/fundamentos/Class07Estaciones.java

--- src/fundamentos/Class31UsoTrabajador.java
+++ src/fundamentos/Class31UsoTrabajador.java
@@ -2,15 +2,30 @@ package fundamentos;
 
 import clases.Director;
 import clases.Trabajador;
+import java.util.Scanner;
 
 public class Class31UsoTrabajador {
 
     public static void main(String[] args) throws Exception {
+        Scanner teclado = new Scanner(System.in);
         Trabajador tra = new Trabajador();
         tra.setNombre("Currante");
         tra.setApellidos("Super currante");
         tra.setEdad(33);
         tra.setSalario(1000);
+        int opcion = -1;
+        while (opcion != 0) {
+            System.out.println("0.- Salir");
+            System.out.println("1.- Ver salario");
+            System.out.println("2.- Ver nombre");
+            String dato = teclado.nextLine();
+            opcion = Integer.parseInt(dato);
+            if (opcion == 1) {
+                System.out.println("Salario " + tra.getSalario());
+            } else if (opcion == 2) {
+                System.out.println("Nombre " + tra.getNombreCompleto());
+            }
+        }
 
         //tra.salarioMinimo = 90000;
         System.out.println("Trabajador " + tra.getNombreCompleto());
